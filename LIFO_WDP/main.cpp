f928391164219deaaac5366ca7af18e7a71f296e
Kasia <71874062+KatarzynaBorkowska@users.noreply.github.com>
2021-05-28 10:08:27 +0200

    Initial Commit

---
+++ LIFO_WDP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LIFO_WDP
+//
+//  Created by Kasia on 28/05/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
